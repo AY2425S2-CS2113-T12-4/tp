e75ddd9031976c8776c022c68dc2d6d1b2c70371
juneja999 <akkij2004@gmail.com>
2025-03-18 01:01:41 +0800

    Update BudgetSummary class

--- src/main/java/seedu/duke/BudgetSummary.java
+++ src/main/java/seedu/duke/BudgetSummary.java
@@ -1,17 +1,25 @@
 package seedu.duke;
 import java.util.Map;
 
-
+/**
+ * this class summarises the user budget by providing total expenses and spending limit.
+ * this can summarise across multiple budgets
+ */
 public class BudgetSummary {
 
     private final BudgetManager budgetManager;
 
-
+    /**
+     * instantiate the class with a budgetManger to access its private variable "budgets"
+     * @param budgetManager is taken in the Duke class (main class)
+     */
     public BudgetSummary(BudgetManager budgetManager) {
         this.budgetManager = budgetManager;
     }
 
-
+    /**
+     * this is the method called to get the summary printed
+     */
     public void summariseBudget(){
             Map<String, Budget> budgets = budgetManager.getBudgets(); // Access the budgets map
 
