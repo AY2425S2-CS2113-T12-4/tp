0f8661ad32c1cc897b0f5fd54d884f43904b2ecc
ae-24 <115100656+ae-24@users.noreply.github.com>
2025-03-30 21:11:28 +0800

    Update runtest.bat

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -16,4 +16,11 @@ java -jar %jarloc% < ..\..\text-ui-test\input.txt > ..\..\text-ui-test\ACTUAL.TX
 
 cd ..\..\text-ui-test
 
-FC ACTUAL.TXT EXPECTED.TXT >NUL && ECHO Test passed! || Echo Test failed!
+:: Normalize the ACTUAL.TXT and EXPECTED.TXT to remove trailing spaces and newlines
+powershell -Command "Get-Content ACTUAL.TXT | ForEach-Object {$_ -replace '\s+$',''} | Set-Content ACTUAL-NORMALIZED.TXT"
+powershell -Command "Get-Content EXPECTED.TXT | ForEach-Object {$_ -replace '\s+$',''} | Set-Content EXPECTED-NORMALIZED.TXT"
+
+:: Compare the normalized files
+FC ACTUAL-NORMALIZED.TXT EXPECTED-NORMALIZED.TXT >NUL && ECHO Test passed! || ECHO Test failed!
+
+popd
