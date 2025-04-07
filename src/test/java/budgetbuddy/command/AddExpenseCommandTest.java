52b7308ed5ce3b08e7312cf3d8e0fb9ae1f3036c
juneja999 <akkij2004@gmail.com>
2025-04-07 21:14:56 +0800

    Add tests for AddExpense Command

--- src/test/java/budgetbuddy/command/AddExpenseCommandTest.java
+++ src/test/java/budgetbuddy/command/AddExpenseCommandTest.java
@@ -1,6 +1,5 @@
 package budgetbuddy.command;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import budgetbuddy.model.BudgetManager;
@@ -9,6 +8,8 @@ import budgetbuddy.model.Budget;
 import budgetbuddy.model.Expense;
 import java.util.List;
 import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class AddExpenseCommandTest {
 
@@ -42,5 +43,56 @@ public class AddExpenseCommandTest {
             System.out.println("Invalid input");
         }
     }
+
+    @Test
+    public void testExecute_validFullInput_addsCorrectly() throws InvalidInputException {
+        String description = "add 25.5 c/Food d/Brunch t/Apr 06 2025 at 10:30";
+        addExpenseCommand = new AddExpenseCommand(description);
+        addExpenseCommand.execute(budgetManager);
+
+        Budget overallBudget = budgetManager.getBudgets().get("Overall");
+        Expense expense = overallBudget.getExpenses().get(0);
+
+        assertEquals(25.5, expense.getAmount());
+        assertEquals("Brunch", expense.getDescription());
+        assertEquals("Apr 06 2025 at 10:30", expense.getDateTimeString());
+    }
+    @Test
+    public void testExecute_invalidTimeFormat_usesSystemTime() throws InvalidInputException {
+        String description = "add 100 c/Transport d/TaxiRide t/06-April-2025";
+        addExpenseCommand = new AddExpenseCommand(description);
+        addExpenseCommand.execute(budgetManager);
+
+        Budget overallBudget = budgetManager.getBudgets().get("Overall");
+        Expense expense = overallBudget.getExpenses().get(0);
+
+        assertEquals(100, expense.getAmount());
+        assertEquals("TaxiRide", expense.getDescription());
+        assertNotEquals("06-April-2025", expense.getDateTimeString()); //System time should be registered
+    }
+
+    @Test
+    public void testExecute_missingAmount_throwsInvalidInputException() {
+        String description = "add c/Food d/Lunch t/Apr 06 2025 at 12:00";
+        addExpenseCommand = new AddExpenseCommand(description);
+
+        try {
+            addExpenseCommand.execute(budgetManager);
+        } catch (InvalidInputException | NumberFormatException e) {
+            assertEquals(0, budgetManager.getBudgets().get("Overall").getExpenses().size());
+        }
+    }
+    @Test
+    public void testExecute_unknownCategory_addsToOverallOnly() throws InvalidInputException {
+        String description = "add 40 c/UnknownCategory d/Snack t/Apr 06 2025 at 15:00";
+        addExpenseCommand = new AddExpenseCommand(description);
+        addExpenseCommand.execute(budgetManager);
+
+        Budget overallBudget = budgetManager.getBudgets().get("Overall");
+        assertEquals(1, overallBudget.getExpenses().size());
+        assertFalse(budgetManager.getBudgets().containsKey("UnknownCategory")); // UnknownCategory should not be created
+    }
+
+
 
 }
